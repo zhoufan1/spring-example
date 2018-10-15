6901d42bea9577a73e1b4afdbcbe895e8d70a8a7
zhou.fan <wodeer33@gmail.com>
2018-10-15 18:04:04 +0800

    update

--- user/src/main/java/com/example/user/repositorys/UserRepository.java
+++ user/src/main/java/com/example/user/repositorys/UserRepository.java
@@ -4,8 +4,17 @@ import com.example.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * @author zhou.fan
+ * 用户访问数据
+ */
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    @Transactional(readOnly = true)
+    /**
+     * 根据用户名称查询
+     * @param userName 用户名称
+     * @return User
+     */
+    @Transactional(readOnly = true ,rollbackFor = Exception.class)
     User findUserByUserName(String userName);
 }
